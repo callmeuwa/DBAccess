596da5c9b6084d8cdf5193251379ffa1c85b23d4
Joshua Blaylock <joshuasimpsonblaylock@gmail.com>
2018-05-01 15:15:35 -0700

    First commit

---
+++ Tools/DataMapper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Data;
+
+
+namespace DbAccess.Tools
+{
+    public class DataMapper<T> where T : class
+    {
+        private static readonly DataMapper<T> _instance = new DataMapper<T>();
+        PropertyInfo[] properties;
+
+        private DataMapper()
+        {
+            properties = typeof(T).GetProperties();
+        }
+        static DataMapper() { }
+
+        public static DataMapper<T> Instance { get { return _instance; } }
+
+        public T MapToObject(IDataReader reader)
+        {
+            IEnumerable<string> columns = reader.GetSchemaTable().Rows.Cast<DataRow>().Select(c => c["columnName"].ToString().ToLower()).ToList();
+
+            T obj = Activator.CreateInstance<T>();
+
+            foreach (PropertyInfo pinfo in properties)
+            {
+                if (columns.Contains(pinfo.Name.ToLower()))
+                {
+                    if(reader[pinfo.Name] != DBNull.Value)
+                    {
+                        if(reader[pinfo.Name].GetType() == typeof(decimal))
+                        {
+                            pinfo.SetValue(obj, reader.GetDouble(pinfo.Name));
+                        }
+                        else
+                        {
+                            pinfo.SetValue(obj, (reader.GetValue(reader.GetOrdinal(pinfo.Name)) ?? null), null);
+                        }
+                    }
+                }
+            }
+
+            return obj;
+        }
+
+    }
+
+    public static class DataHelper
+    {
+        public static double GetDouble(this IDataReader reader, string columnName)
+        {
+            double dbl = 0;
+            double.TryParse(reader[columnName].ToString(), out dbl);
+            return dbl;
+        }
+
+        public static double GetDouble(this IDataReader reader, int columnIndex)
+        {
+            double dbl = 0;
+            double.TryParse(reader[columnIndex].ToString(), out dbl);
+            return dbl;
+        }
+    }
+}
